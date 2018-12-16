aa1b1039dec8c69790f11341acd9998db1166eb3
drelaptop <li_cheng_zhe@163.com>
2018-12-16 21:49:41 +0800

    update AppDelegate

--- tests/js-tests/project/Classes/AppDelegate.cpp
+++ tests/js-tests/project/Classes/AppDelegate.cpp
@@ -1,61 +1,40 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
 #include "AppDelegate.h"
 
 #include "cocos2d.h"
 #include "audio/include/SimpleAudioEngine.h"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_3d_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_3d_extension_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_builder_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_extension_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_network_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_navmesh_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_physics3d_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_spine_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_studio_auto.hpp"
-#include "scripting/js-bindings/auto/jsb_cocos2dx_ui_auto.hpp"
-#include "scripting/js-bindings/manual/3d/jsb_cocos2dx_3d_manual.h"
-#include "scripting/js-bindings/manual/ScriptingCore.h"
-#include "scripting/js-bindings/manual/chipmunk/js_bindings_chipmunk_registration.h"
-#include "scripting/js-bindings/manual/cocos2d_specifics.hpp"
-#include "scripting/js-bindings/manual/cocosbuilder/cocosbuilder_specifics.hpp"
-#include "scripting/js-bindings/manual/cocosbuilder/js_bindings_ccbreader.h"
-#include "scripting/js-bindings/manual/cocostudio/jsb_cocos2dx_studio_manual.h"
-#include "scripting/js-bindings/manual/extension/jsb_cocos2dx_extension_manual.h"
-#include "scripting/js-bindings/manual/jsb_opengl_registration.h"
-#include "scripting/js-bindings/manual/localstorage/js_bindings_system_registration.h"
-#include "scripting/js-bindings/manual/navmesh/jsb_cocos2dx_navmesh_manual.h"
-#include "scripting/js-bindings/manual/network/XMLHTTPRequest.h"
-#include "scripting/js-bindings/manual/network/jsb_socketio.h"
-#include "scripting/js-bindings/manual/network/jsb_websocket.h"
-#include "scripting/js-bindings/manual/physics3d/jsb_cocos2dx_physics3d_manual.h"
-#include "scripting/js-bindings/manual/spine/jsb_cocos2dx_spine_manual.h"
-#include "scripting/js-bindings/manual/ui/jsb_cocos2dx_ui_manual.h"
-#include "js_DrawNode3D_bindings.h"
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-#include "scripting/js-bindings/manual/platform/android/CCJavascriptJavaBridge.h"
-#elif (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
-#include "scripting/js-bindings/manual/platform/ios/JavaScriptObjCBridge.h"
-#endif
 
-#include "js_Effect3D_bindings.h"
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-#include "scripting/js-bindings/auto/jsb_cocos2dx_experimental_webView_auto.hpp"
-#include "scripting/js-bindings/manual/experimental/jsb_cocos2dx_experimental_webView_manual.h"
-#endif
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-#include "scripting/js-bindings/auto/jsb_cocos2dx_experimental_video_auto.hpp"
-#include "scripting/js-bindings/manual/experimental/jsb_cocos2dx_experimental_video_manual.h"
-#endif
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
-#include "scripting/js-bindings/auto/jsb_cocos2dx_audioengine_auto.hpp"
-#endif
+#include "cocos/scripting/js-bindings/manual/ScriptingCore.h"
+#include "cocos/scripting/js-bindings/manual/jsb_module_register.hpp"
+#include "cocos/scripting/js-bindings/manual/jsb_global.h"
+#include "cocos/scripting/js-bindings/jswrapper/SeApi.h"
 
 USING_NS_CC;
-USING_NS_CC_EXT;
 using namespace CocosDenshion;
 
 AppDelegate::AppDelegate()
@@ -80,93 +59,41 @@ bool AppDelegate::applicationDidFinishLaunching()
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-#if(CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-        glview = cocos2d::GLViewImpl::create("js-tests");
-#else
-        glview = cocos2d::GLViewImpl::createWithRect("js-tests", Rect(0,0,900,640));
-#endif
+        glview = GLViewImpl::createWithRect("HelloJavascript", cocos2d::Rect(0,0,900,640));
         director->setOpenGLView(glview);
     }
 
     // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0f / 60);
+    director->setAnimationInterval(1.0 / 60);
 
     ScriptingCore* sc = ScriptingCore::getInstance();
-    sc->addRegisterCallback(register_all_cocos2dx);
-    sc->addRegisterCallback(register_cocos2dx_js_core);
-    sc->addRegisterCallback(jsb_register_system);
-
-    sc->addRegisterCallback(register_all_cocos2dx_extension);
-    sc->addRegisterCallback(register_all_cocos2dx_extension_manual);
-
-    sc->addRegisterCallback(jsb_register_chipmunk);
-    sc->addRegisterCallback(JSB_register_opengl);
-
-    sc->addRegisterCallback(MinXmlHttpRequest::_js_register);
-    sc->addRegisterCallback(register_jsb_websocket);
-	sc->addRegisterCallback(register_jsb_socketio);
-    sc->addRegisterCallback(register_all_cocos2dx_network);
-
-    sc->addRegisterCallback(register_all_cocos2dx_builder);
-    sc->addRegisterCallback(register_CCBuilderReader);
-
-    sc->addRegisterCallback(register_all_cocos2dx_ui);
-    sc->addRegisterCallback(register_all_cocos2dx_ui_manual);
-    sc->addRegisterCallback(register_all_cocos2dx_studio);
-    sc->addRegisterCallback(register_all_cocos2dx_studio_manual);
-
-    sc->addRegisterCallback(register_all_cocos2dx_spine);
-    sc->addRegisterCallback(register_all_cocos2dx_spine_manual);
-
-    sc->addRegisterCallback(register_all_cocos2dx_3d);
-    sc->addRegisterCallback(register_all_cocos2dx_3d_manual);
-
-    sc->addRegisterCallback(register_all_cocos2dx_3d_extension);
-
-#if CC_USE_3D_PHYSICS && CC_ENABLE_BULLET_INTEGRATION
-    sc->addRegisterCallback(register_all_cocos2dx_physics3d);
-    sc->addRegisterCallback(register_all_cocos2dx_physics3d_manual);
-#endif
-
-#if CC_USE_NAVMESH
-	sc->addRegisterCallback(register_all_cocos2dx_navmesh);
-	sc->addRegisterCallback(register_all_cocos2dx_navmesh_manual);
-#endif
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-    sc->addRegisterCallback(JavascriptJavaBridge::_js_register);
-#elif (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
-    sc->addRegisterCallback(JavaScriptObjCBridge::_js_register);
-#endif
-
-    sc->addRegisterCallback(register_DrawNode3D_bindings);
-    sc->addRegisterCallback(register_Effect3D_bindings);
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) && !defined(CC_TARGET_OS_TVOS)
-    sc->addRegisterCallback(register_all_cocos2dx_experimental_webView);
-    sc->addRegisterCallback(register_all_cocos2dx_experimental_webView_manual);
-    sc->addRegisterCallback(register_all_cocos2dx_experimental_video);
-    sc->addRegisterCallback(register_all_cocos2dx_experimental_video_manual);
-#endif
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
-    sc->addRegisterCallback(register_all_cocos2dx_audioengine);
-#endif
-
-    sc->start();
-    sc->runScript("script/jsb_boot.js");
-#if defined(COCOS2D_DEBUG) && (COCOS2D_DEBUG > 0)
-    sc->enableDebugger();
-#endif
-
     ScriptEngineManager::getInstance()->setScriptEngine(sc);
 
-    sc->runScript("main.js");
+    se::ScriptEngine* se = se::ScriptEngine::getInstance();
+
+    jsb_set_xxtea_key("");
+    jsb_init_file_operation_delegate();
+
+#if defined(COCOS2D_DEBUG) && (COCOS2D_DEBUG > 0)
+    // Enable debugger here
+    jsb_enable_debugger("0.0.0.0", 5086);
+#endif
+
+    se->setExceptionCallback([](const char* location, const char* message, const char* stack){
+        // Send exception information to server like Tencent Bugly.
+
+    });
+
+    jsb_register_all_modules();
+
+    se->start();
+
+    jsb_run_script("main.js");
 
     return true;
 }
 
-// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
     auto director = Director::getInstance();
